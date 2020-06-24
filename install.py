9651a63dc19ab9e865d98378c8534f4e53ff6139
Ubuntu <ubuntu@ip-172-31-2-196.cn-north-1.compute.internal>
2020-06-24 06:38:27 +0000

    调整目录

--- install.py
+++ install.py
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp bin/ipmstream /usr/local/bin
-cp libstdc++.so.6.0.21 /usr/lib/x86_64-linux-gnu/
+cp bin/libstdc++.so.6.0.21 /usr/lib/x86_64-linux-gnu/
 cd /usr/lib/x86_64-linux-gnu/
 ln -sf libstdc++.so.6.0.21 /usr/lib/x86_64-linux-gnu/libstdc++.so.6
 ldconfig
